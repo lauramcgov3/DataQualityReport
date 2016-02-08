e34c31970416f367704dd41ed97aac08004c041b
Laura <laura@lauras-air.ict.ad.dit.ie>
2016-02-08 13:54:14 +0000

    first commit

---
+++ QualityReport.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 13:07:44 2016
+
+@author: laura
+@student number: C12431378
+@title: Assignment 1: Data Quality Report
+"""
+
+#Import required libraries
+import pandas as pd
+
+#Read in data files
+featureNames = pd.read_csv('./data/featureNames.txt')
+DataSet = pd.read_csv('./data/DataSet.txt')
