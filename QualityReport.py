750ccc5329ece4968c26121833fc328e7c94fb9b
Laura <laura@Lauras-MacBook-Air.local>
2016-02-14 17:05:36 +0000

    Latest update

--- QualityReport.py
+++ QualityReport.py
@@ -9,8 +9,12 @@ Created on Mon Feb  8 13:07:44 2016
 
 # Import all libraries needed
 import pandas as pd 
-from pandas import DataFrame, read_csv
+#from pandas import DataFrame, read_csv
 import matplotlib.pyplot as plt
+import numpy as np
+from numpy import nan
+from scipy.stats.mstats import mode
+
 
 class DataQuality(object):
     
@@ -27,8 +31,10 @@ class DataQuality(object):
         """
         #These variables are created using self.name_of_variable notation
         self.data = self.get_data(dataset) 
-        self.cat = self.cat_data(self.data)
-        self.cont = self.cont_data(self.data)
+        self.catData = self.cat_data(self.data)
+        self.contData = self.cont_data(self.data)
+        self.cont_table()
+        self.cat_table()
 
         
     def get_data(self, dataset):    
@@ -40,7 +46,7 @@ class DataQuality(object):
         
         This method essentially creates the Analytics Basics Table.
         """
-
+        
         ins = open('./data/featureNames.txt', "r")
         
         colum_headings =[]
@@ -54,7 +60,7 @@ class DataQuality(object):
                                  header=None,
                                  names=colum_headings)
         show_headers.columns = colum_headings
-        #print(show_headers)
+        
         return(show_headers)
         
 
@@ -67,7 +73,7 @@ class DataQuality(object):
         stored in self.cat
         """
         # Read in catFeatures.txt (text file of the categorical features)
-        catFile = open('./data/catFeatures.txt', "r")
+        catFile = open('./odata/catFeatures.txt', "r")
         
         # Create array to store categorical features
         cat = []
@@ -76,12 +82,12 @@ class DataQuality(object):
         for line in catFile:
             line = line.strip()
             cat.append(line)
-
+        
         # Create DataFrame with columns of categorical data
         getCatData = pd.DataFrame(lst, columns=cat)
-        getCatData.describe()
+        getCatData.to_csv('./odata/catData.csv',index=True,header=True)
         return(getCatData)
-               
+        
     
     
     def cont_data(self, lst):
@@ -93,7 +99,7 @@ class DataQuality(object):
         stored in self.cont
         """        
         # Read in contFeatures.txt (text file of the continuous features)
-        contFile = open('./data/contFeatures.txt', "r")
+        contFile = open('./odata/contFeatures.txt', "r")
         
         # Create array to store continuous features
         cont = []
@@ -105,15 +111,151 @@ class DataQuality(object):
         
         # Create DataFrame with columns of continuous data
         getContData = pd.DataFrame(lst, columns=cont)
+        getContData.to_csv('./odata/contData.csv',index=True,header=True)
         return(getContData)
         
+    
+        
+    def cont_table(self):
+         
+        # Read in contFeatures.txt (text file of the continuous features)
+        indexFile = open('./odata/contFeatures.txt', "r")
+        
+        # Create array to store continuous features
+        contFeatures = []
+        
+        # Add values to array
+        for line in indexFile:
+            line = line.strip()
+            contFeatures.append(line)
+        
+        inputContData = pd.read_csv('./odata/contData.csv',
+                        sep=',',
+                        header=0,
+                        index_col=False,
+                        dayfirst=True,
+                        tupleize_cols=False,
+                        error_bad_lines=True,
+                        warn_bad_lines=True,
+                        skip_blank_lines=True
+                        )
+        # Selects only columns wanted for report (age, fnlwgt etc)
+        contData = inputContData[contFeatures]
+         
+        # Count 
+        count = pd.DataFrame(contData.count(), columns=['Count'])
+        
+        # Calculate missing %        
+        miss_data = contData.isnull().sum()
+        miss_pct = pd.DataFrame((miss_data/count.Count)*100, columns =[' % Miss.'])
+        
+        # Cardinality 
+        unique_value_counts = pd.DataFrame(columns=['Card.'])
+        for v in list(contData.columns.values):
+            unique_value_counts.loc[v] = [contData[v].nunique()]
+        
+        # Min
+        minimum_values = pd.DataFrame(columns=['Min'])
+        for v in list(contData.columns.values):
+            minimum_values.loc[v] = [contData[v].min()]
+            
+        # 1st Quartile
+        f_qrt = pd.DataFrame(columns=['1st Qrt.'])
+        for v in list(contData.columns.values):
+            f_qrt.loc[v] = [contData[v].quantile(.25)]
+        
+        # Mean        
+        mean = pd.DataFrame(contData.mean(), columns=['Mean'])
+        
+        # Median
+        median = pd.DataFrame(contData.median(), columns=['Median'])
+        
+        # 3rd Quartile
+        t_qrt = pd.DataFrame(columns=['3rd Qrt.'])
+        for v in list(contData.columns.values):
+            t_qrt.loc[v] = [contData[v].quantile(.75)]
+           
+        # Max
+        max_values = pd.DataFrame(columns=['Max'])
+        for v in list(contData.columns.values):
+            max_values.loc[v] = [contData[v].max()]
+       
+        # Standard Dev
+        std_dev = pd.DataFrame(contData.std(), columns=['Std. Dev.'])
+        
+              
+        # Final report for continuous data      
+        cont_report = pd.DataFrame((count.join(miss_pct).join(unique_value_counts).join(minimum_values).join(f_qrt).join(mean).join(median).join(t_qrt).join(max_values).join(std_dev)))
+        cont_report.index.name = 'Feature'
+        
+        cont_report.to_csv('./data/ContReport.csv')
+        
+    def cat_table (self):
+        
+        # Read in catFeatures.txt (text file of the categorical features)
+        catFile = open('./odata/catFeatures.txt', "r")
+        
+        # Create array to store categorical features
+        catFeatures = []
+        
+        # Add features to array
+        for line in catFile:
+            line = line.strip()
+            catFeatures.append(line)
+            
+        inputCatData = pd.read_csv('./odata/catData.csv',
+                        sep=',',
+                        header=0,
+                        index_col=False,
+                        dayfirst=True,
+                        tupleize_cols=False,
+                        error_bad_lines=True,
+                        warn_bad_lines=True,
+                        skip_blank_lines=True
+                        )
+        # Selects only columns wanted for report
+        catData = pd.DataFrame(inputCatData[catFeatures])
+        catData.replace(to_replace='?', value=nan)
+        print(catData)
+        
+        # Count 
+        count = pd.DataFrame(catData.count(), columns=['Count'])
+        print(count)
+        
+        # Calculate missing %        
+        miss_data = catData.isnull().sum()
+        miss_pct = pd.DataFrame((miss_data/count.Count)*100, columns =[' % Miss.'])
+        print(miss_pct)        
+        
+        # Cardinality 
+        unique_value_counts = pd.DataFrame(columns=['Card.'])
+        for v in list(catData.columns.values):
+            unique_value_counts.loc[v] = [catData[v].nunique()]
+        print(unique_value_counts)
+        
+        # Mode
+        mode_values = pd.DataFrame(catData.mode().transpose())
+        mode_values.columns = ['Mode']
+        
+        # Mode frequency
+        f = lambda catData: mode(catData, axis=None)[0]
+        
+        mode_freq = catData.groupby(level=0).apply(f)
+        print(mode_freq)
+        
+        # 2nd Mode
         
         
+        
+        
+        
+         
 def main():
     
 
     dataset = './data/DataSet.txt'
     My_DQR = DataQuality(dataset)
+    
 
 
 # Call main ()
