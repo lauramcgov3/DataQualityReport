29350049e2b7cf1ccb81d938145e4df5b8505f47
Laura <laura@Lauras-MacBook-Air.local>
2016-02-15 11:37:50 +0000

    Final code

--- QualityReport.py
+++ QualityReport.py
@@ -9,11 +9,7 @@ Created on Mon Feb  8 13:07:44 2016
 
 # Import all libraries needed
 import pandas as pd 
-#from pandas import DataFrame, read_csv
-import matplotlib.pyplot as plt
-import numpy as np
 from numpy import nan
-from scipy.stats.mstats import mode
 from collections import Counter
 
 
@@ -29,8 +25,11 @@ class DataQuality(object):
         
         """
         Constructor method
+        
+        Parameters (self, dataset)
         """
-        #These variables are created using self.name_of_variable notation
+        # These variables are created using self.name_of_variable notation
+        # Functions of DataQuality
         self.data = self.get_data(dataset) 
         self.catData = self.cat_data(self.data)
         self.contData = self.cont_data(self.data)
@@ -46,6 +45,9 @@ class DataQuality(object):
         make the data more readable. 
         
         This method essentially creates the Analytics Basics Table.
+        
+        Parameters (self, dataset)
+        Returns (DataFrame: abt_file)
         """
         
         ins = open('./data/featureNames.txt', "r")
@@ -56,13 +58,14 @@ class DataQuality(object):
             line = line.strip()
             colum_headings.append(line)
             
-        show_headers = pd.read_csv('./data/DataSet.txt', 
+        abt_file = pd.read_csv('./data/DataSet.txt', 
                                  sep=',', 
                                  header=None,
                                  names=colum_headings)
-        show_headers.columns = colum_headings
+        abt_file.columns = colum_headings
         
-        return(show_headers)
+        abt_file.to_csv('./odata/abtFile.csv')
+        return(abt_file)
         
 
     def cat_data(self, lst):
@@ -72,6 +75,9 @@ class DataQuality(object):
         from the data set and returns a new data set with just
         the values for those colums. The returned data set is
         stored in self.cat
+        
+        Parameters (self, Dataframe: lst)
+        Returns (DataFrame: getCatData)
         """
         # Read in catFeatures.txt (text file of the categorical features)
         catFile = open('./odata/catFeatures.txt', "r")
@@ -98,6 +104,9 @@ class DataQuality(object):
         from the data set and returns a new data set with just
         the values for those colums. The returned data set is
         stored in self.cont
+        
+        Parameters (self, Dataframe: lst)
+        Returns (DataFrame: getContData)
         """        
         # Read in contFeatures.txt (text file of the continuous features)
         contFile = open('./odata/contFeatures.txt', "r")
@@ -118,6 +127,30 @@ class DataQuality(object):
     
         
     def cont_table(self):
+        
+        """
+        The cont_table method is used to create the continuous
+        data quality table. It reads in the continuous data 
+        feature names, the continuous data set and combines 
+        them to create the ABT.
+        
+        It then performs functions to get:
+            * Count
+            * % Missing data
+            * Cardinality
+            * Minimum
+            * First quartile
+            * Mean
+            * Median
+            * Third quartile
+            * Maximum
+            * Standard Deviation
+            
+        It then prints the table and exports to a csv file.
+        
+        Parameters(self)
+        """
+            
          
         # Read in contFeatures.txt (text file of the continuous features)
         indexFile = open('./odata/contFeatures.txt', "r")
@@ -198,12 +231,36 @@ class DataQuality(object):
                                     
         cont_report.index.name = 'Feature'
         
+        # Print data report
         print("\n Continuous Features Data Quality Table \n\n")        
         print(cont_report)
         
+        # Export to csv file
         cont_report.to_csv('./data/ContReport.csv')
         
     def cat_table (self):
+        
+        """
+        The cat_table method is used to create the categorical
+        data quality table. It reads in the categorical data 
+        feature names, the ccategorical data set and combines 
+        them to create the ABT.
+        
+        It then performs functions to get:
+            * Count
+            * % Missing data
+            * Cardinality
+            * Mode
+            * Mode Frequency
+            * Mode %
+            * Second Mode
+            * Second Mode Frequency
+            * Second Mode %
+            
+        It then prints the table and exports to a csv file.
+        
+        Parameters(self)
+        """        
         
         # Read in catFeatures.txt (text file of the categorical features)
         catFile = open('./odata/catFeatures.txt', "r")
@@ -227,7 +284,7 @@ class DataQuality(object):
             line = line.strip()
             catHeadings.append(line)
             
-            
+        # Read in categorical data   
         inputCatData = pd.read_csv('./odata/catData.csv',
                         sep=',',
                         header=0,
@@ -240,34 +297,32 @@ class DataQuality(object):
                         names = catFeatures
                         )
                         
-        catDataCols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        
-        catRows = list()
         
         # Selects only columns wanted for report
         catData = pd.DataFrame(inputCatData[catFeatures])
         catData.replace(to_replace='?', value=nan)
-        #print(catData)
         
         # Count 
         count = pd.DataFrame(catData.count(), columns=['Count'])
-        # print(count)
         
         # Calculate missing %        
         miss_data = catData.isnull().sum()
-        miss_pct = pd.DataFrame((miss_data/count.Count)*100, columns =[' % Miss.'])
-        # print(miss_pct)        
+        miss_pct = pd.DataFrame((miss_data/count.Count)*100, columns =[' % Miss.'])      
         
         # Cardinality 
         unique_value_counts = pd.DataFrame(columns=['Card.'])
         for v in list(catData.columns.values):
             unique_value_counts.loc[v] = [catData[v].nunique()]
-        # print(unique_value_counts)
+
+        # Modes, modde frequencies and mode %
         
-        # Mode
-        # mode_values = pd.DataFrame(catData.mode().transpose())
-        # mode_values.columns = ['Mode']
+        # Number columns 
+        catDataCols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         
+        # List to store data
+        catRows = list()
+        
+        # Loop to access all data and apply functions to get values
         for i in catDataCols:
             data = Counter(inputCatData[catFeatures[i]].tolist())
             modes = data.most_common(2)
@@ -281,8 +336,10 @@ class DataQuality(object):
                 round((modes[1][1] / inputCatData[catFeatures[i]].count() * 100), 4)#2nd mode %,
                 ]
             )
-            
+        # Structure Data    
         mode_maths = pd.DataFrame(catRows, columns = ['Feature', 'Mode', 'Mode Freq.', 'Mode %', '2nd Mode', '2nd Mode Freq.', '2nd Mode %' ], index=catFeatures)
+        
+        # Create DQR style structure        
         mode_values = pd.DataFrame(mode_maths[catHeadings], index=catFeatures)
         
         # Final report for continuous data      
@@ -293,18 +350,25 @@ class DataQuality(object):
                                     
         cat_report.index.name = 'Feature'
         
+        # Print final categorical data report
         print(" \n Categorical Features Data Quality Table \n\n")        
         print(cat_report)
         
+        # Export to csv
         cat_report.to_csv('./data/CatReport.csv')
         
         
          
 def main():
     
-
+    """
+    Main function of program
+    """
+    # Original csv file 
     dataset = './data/DataSet.txt'
-    My_DQR = DataQuality(dataset)
+    
+    #Call Data Quality and pass it the dataset
+    DataQuality(dataset)
     
 
 
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# DataQualityReport
+Python, Anacondas, Spyder, pandas, numpy and collections (Counter).
+
+This program is used to generate a Data Quality report for Data Analytics.
+The program reads in two files (DataSet.txt and featureNames.txt) from the
+data directory of the project (./data).
+These two files are used to create an Analytics Basics Table (abt_file)
+which is stored in the other data directory of the project (./odata).
+
+The ABT is then split into categorical and continuous data sets 
+(catData.csv and contData.csv). These two data sets are then used to create 
+data quality tables for both continuous and categorical data features.
+Both Data Quality tables are then printed by the program and output to csv
+files in the data diretory of the project (./data) in files named:
+CatReport.csv
+ContReport.csv
